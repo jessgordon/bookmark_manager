f899a6d1284579ea50310ab9ca1752302b9929c8
Inimesh <ijmeshoulam@gmail.com>
2021-12-07 16:32:02 +0000

    set up the reusable test database

--- README.md
+++ README.md
@@ -57,3 +57,8 @@ I'd like to be able to restrict users to manage only their own bookmarks.
 - Connect to the database using the pqsl command \c bookmark_manager;
 - Run the query we have saved in the file 01_create_bookmarks_table.sql
 
+For rspec
+- Connect to psql
+- Create the database using the psql command CREATE DATABASE bookmark_manager_test;
+- Connect to the database using the pqsl command \c bookmark_manager_test;
+- Run the query we have saved in the file 01_create_bookmarks_table.sql
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -2,6 +2,12 @@ require 'bookmark'
 
 describe Bookmark do
   it 'should return a list of the bookmarks' do
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+
+    connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.google.com/');")
+    connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com/');")
+    connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.destroyallsoftware.com');")
+
     expect(Bookmark.all).to include 'http://www.google.com/'
     expect(Bookmark.all).to include 'http://www.makersacademy.com/'
     expect(Bookmark.all).to include 'http://www.destroyallsoftware.com'
